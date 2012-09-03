b1e386e2b574abddd80da44e890a3fdbcf6fb525
Juan Carlos Cerrada Garcia <jccerrada@telefonica.net>
2012-09-03 22:34:59 +0200

    Primera Subida

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c79c753d385725d319ff8c79fd7941c8ee5d88db40201f0d44fc53c4f6a6eab82164e43adb919e6a5b48cea879bb721ff8d11bc6913be30fe89b38e8aca6cb7e'
